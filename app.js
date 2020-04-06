c4a4b19a8939667a9cd6e821512b5aa048c0d715
Imran Kheraj <ikheraj17@gmail.com>
2020-04-06 17:47:30 -0500

    refactored GroceryList with props

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 // TODO
-var GroceryList = () => (
+/*var GroceryList = () => (
     <ul>
       <li>Milk</li>
       <li>Eggs</li>  
     </ul>
+)*/
+
+/*var GroceryList = () => (
+    <div>
+        <h2>My Grocery List</h2>
+           <ul>
+             <Milk />
+             <Eggs />
+           </ul>
+    </div>
 )
 
-ReactDOM.render(<GroceryList />, document.getElementById("app"))
+var Milk = () => (<li>Milk</li>)
+     
+
+
+var Eggs = () => (<li>Eggs</li>)*/
+     
+
+var Heading = () => (
+    <h2>Grocery List Refactor</h2>
+);
+
+var GroceryListItem = (props) => (
+<ul>
+    <li>{props.item[0]}</li>
+    <li>{props.item[1]}</li>
+</ul>
+);
+
+var GroceryList = () => (
+    <div>
+        <Heading />
+        <GroceryListItem item={['Milk', 'Eggs']}/>
+    </div>
+)
+
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
